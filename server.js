78d191e0b3b00c3acdef18b2e92e51c808d32bb6
Jordin Bryant <jordintaylor095@gmail.com>
2022-11-05 18:30:03 -0400

    added client/build

--- server.js
+++ server.js
@@ -4,9 +4,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 // Import JSON file
 const projects = require("./projects.json");
-
-
-
+const path = require("path");
 
 
 // mongodb error / success 
@@ -23,15 +21,22 @@ const app = express();
 app.use(cors());
 
 //home route for testing our app
-app.get("/", (req, res) => {
-    res.send("Hello World");
-});
+// app.get("/", (req, res) => {
+//     res.send("Hello World");
+// });
 
 // route for retrieving projects
 app.get("/projects", (req, res) => {
     // send projects via JSON
     res.json(projects);
 });
+// in production 
+// if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"))
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname,"client","build","index.html"))
+    })
+// }
 
 //declare a variable for our port number
 const PORT = process.env.PORT || 4000;
